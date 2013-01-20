cc182bce934c7ce527ea8fdd07724870387530cf
Ken Jung <kenmazy@gmail.com>
2013-01-20 13:13:07 -0800

    Adds num_books variable in results.ejs

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,42 @@ exports.results = function(req, res){
   if( isbn == '' ) {
     res.render('layout', { page: 'results', num: 0 })
   } else {
+    var num_books = 0
+    for( var i=0; i< schools.length; i++ ) {
+      if( isbn in schools[i].has ) {
+        results.push( schools[i] )
+          num_books += schools[i].has[isbn];
+      }
+    }
+
+    res.render('layout', { 
+      page: 'results',
+      title: 'Results',
+      num: results.length,
+      results: results,
+      isbn: isbn,
+      book: book,
+      num_books: num_books
+    });
+  }
+};
+
+/*
+ * POST /donateresults
+ */
+exports.donate_results = function(req, res){
+  var isbn = '';
+  var book = {};
+  for(var i=0; i< fake_books.length; i++ ) {
+    if( req.body.book.title == fake_books[i].title || req.body.book.isbn == fake_books[i].isbn ) {
+      isbn = fake_books[i].isbn
+      book = fake_books[i]
+    }
+  }
+  var results = []
+  if( isbn == '' ) {
+    res.render('layout', { page: 'results', num: 0 })
+  } else {
     for( var i=0; i< schools.length; i++ ) {
       if( isbn in schools[i].has ) {
         results.push( schools[i] )
@@ -30,7 +66,7 @@ exports.results = function(req, res){
 
     res.render('layout', { 
       page: 'results',
-      title: 'Results',
+      title: 'Donate Results',
       num: results.length,
       results: results,
       isbn: isbn,
